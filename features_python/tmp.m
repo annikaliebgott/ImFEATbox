435d41a086d58a7448316e48adb16acde4db2d44
Heiko Strohmeier <heiko.strohmeier@gmail.com>
2017-10-04 15:33:28 +0200

    master branch cleanup

--- features_python/tmp.m
+++
@@ -1,5 +0,0 @@
-I = csvread('testimg.csv');
-plotflag = false;
-addpath('ImFEATbox/LocalFeatures/');
-Out = HarrisF(I, plotflag);
-csvwrite('matlab-out.csv', Out);
--- features_python/tmp.py
+++
@@ -1,9 +0,0 @@
-import ImFEATbox, csv
-import numpy as np
-with open('testimg.csv', 'r') as csvfile:
-	I = np.array(list(csv.reader(csvfile, delimiter=','))).astype(np.float)
-plotflag = False
-out = ImFEATbox.LocalFeatures.harris.cFeatures(I, plotflag)
-with open('python-out.csv', 'wb') as csvfile:
-	writer = csv.writer(csvfile, delimiter=',', escapechar=' ', quoting=csv.QUOTE_NONE)
-	writer.writerow(out)
